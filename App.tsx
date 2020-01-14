163cf76af46210b247a338d945845a1d49398fdf
Taichi Murai <taichimurai0514@gmail.com>
2020-01-14 23:59:04 +0900

    add test Location

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 export default function App() {
+  React.useEffect(() => {
+    // console.log(Location.requestPermissionsAsync())
+    // console.log(Location.getLastKnownPositionAsync())
+    console.log(Location.Accuracy.BestForNavigation)
+    console.log(Location.GeofencingEventType.Exit)
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!ddddd</Text>
-      <Text>ddddd</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World</Text>
     </View>
   );
 }
